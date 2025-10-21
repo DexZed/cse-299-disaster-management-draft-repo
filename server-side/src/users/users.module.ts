ffec04ca1ed411ce62dd4c6668f46b95e3727ac6
DexZed <144045080+DexZed@users.noreply.github.com>
2025-10-21 22:10:15 +0600

    `Refactored user entity and removed redundant schema file`

--- server-side/src/users/users.module.ts
+++ server-side/src/users/users.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User } from './entities/user.entity';
-import { UserSchema } from './schemas/user.schema';
+import { User, UserSchema } from './entities/user.entity';
+
 
 @Module({
   imports:[MongooseModule.forFeature([{
