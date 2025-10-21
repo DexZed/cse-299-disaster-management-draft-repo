c2e0199a5cc35dcbf0e97b5c135aa4b0356621e2
DexZed <144045080+DexZed@users.noreply.github.com>
2025-10-21 21:18:51 +0600

    `Added class-validator and class-transformer dependencies and updated user.schema.ts to use validation decorators`

--- server-side/src/users/schemas/user.schema.ts
+++ server-side/src/users/schemas/user.schema.ts
@@ -1,4 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IsEmail, Matches, MinLength } from 'class-validator';
 import { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
@@ -9,21 +10,28 @@ export enum Role {
   Affected = 'affected',
   Volunteer = 'volunteer',
 }
-
+@Schema({timestamps: true})
 @Schema()
 export class User {
   @Prop({ required: true })
   name: string;
 
-  @Prop({ required: true })
-  
+  @Prop({ required: true, unique: true})
+  @IsEmail()
   email: string;
 
-  @Prop({ required: true })
+  @Prop({ required: true, minlength: 8,match: /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).*$/})
+  @MinLength(8)
+  @Matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).*$/, {
+    message: 'Password must contain at least one uppercase letter, one number, and one special character',
+  })
   password: string;
 
-  @Prop({ required: true })
+  @Prop({ required: true, enum: Role, default: Role.USER })
   role: string;
+
+  @Prop()
+  profileImage?: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
