3ab89cb60f51a136d2e43854947bbcb6704fbf4b
DexZed <144045080+DexZed@users.noreply.github.com>
2025-10-21 21:28:34 +0600

    `Added @nestjs/config dependency and its corresponding lock file entries`

--- server-side/src/app.module.ts
+++ server-side/src/app.module.ts
@@ -2,9 +2,35 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('database.URL'),
+        onConnectionCreate: (connection: Connection) => {
+          connection.on('connected', () => console.log('connected'));
+          connection.on('open', () => console.log('open'));
+          connection.on('disconnected', () => console.log('disconnected'));
+          connection.on('reconnected', () => console.log('reconnected'));
+          connection.on('disconnecting', () => console.log('disconnecting'));
+          return connection;
+        },
+      }),
+    }),
+
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
