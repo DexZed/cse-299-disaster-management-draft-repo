d66e10a323880294d14531c5b0cec9fddc494dc2
DexZed <144045080+DexZed@users.noreply.github.com>
2025-10-21 22:03:12 +0600

    `Added nestjs-zod and zod dependencies, updated package.json and pnpm-lock.yaml, and modified user DTO and service to use Zod validation and Mongoose for MongoDB interactions.`

--- server-side/src/users/users.service.ts
+++ server-side/src/users/users.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model, Types } from 'mongoose';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<User>){}
+
+  async create(createUserDto: CreateUserDto) {
+     const createdUser = new this.userModel(createUserDto);
+    return await createdUser.save();
+
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async  findAll() {
+    return await this.userModel.find();
+
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+ const params = new Types.ObjectId(id);
+    return await this.userModel.findById(params);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+
+ update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
   remove(id: number) {
     return `This action removes a #${id} user`;
   }
+
 }
