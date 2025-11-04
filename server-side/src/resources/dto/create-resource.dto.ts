a4db48d8203156afe78be82424abc8d92bc81ab0
DexZed <144045080+DexZed@users.noreply.github.com>
2025-11-04 14:14:02 +0600

    `Added validation schema for CreateResourceDto using Zod and NestJS-Zod`

--- server-side/src/resources/dto/create-resource.dto.ts
+++ server-side/src/resources/dto/create-resource.dto.ts
@@ -1,1 +1,15 @@
-export class CreateResourceDto {}
+import {z} from 'zod';
+import {createZodDto} from 'nestjs-zod';
+
+const ResourcesSchema = z.object({
+    name: z.string(),
+    description: z.string().optional(),
+    quantity: z.number(),
+    status: z.enum(['In-Use', 'Assigned', 'Out of Stock', 'Available']),
+    location: z.string(),
+    expiryDate: z.date().optional(),
+
+});
+
+
+export class CreateResourceDto extends createZodDto(ResourcesSchema){}
