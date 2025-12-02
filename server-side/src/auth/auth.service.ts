9c43a73a8a6f473790da25c0fc9e78955edd259f
DexZed <144045080+DexZed@users.noreply.github.com>
2025-12-02 07:59:17 +0600

    `Update import paths for User entity to src/users/entities/user.entity`

--- server-side/src/auth/auth.service.ts
+++ server-side/src/auth/auth.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User, UserDocument } from '../user/schemas/user.schema';
 import * as jwt from 'jsonwebtoken';
 import * as bcrypt from 'bcrypt';
+import { User, UserDocument } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class AuthService {
--- server-side/src/auth/seed.service.ts
+++ server-side/src/auth/seed.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User, UserDocument } from '../user/schemas/user.schema';
 import { AuthService } from '../auth/auth.service';
+import { User, UserDocument } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class SeedService implements OnModuleInit {
