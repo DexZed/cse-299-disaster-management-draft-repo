b5bb03b723f3f25ace62551af031f3f577ac9bda
DexZed <144045080+DexZed@users.noreply.github.com>
2025-12-03 07:28:27 +0600

    `Updated API endpoint URLs in README.md and corrected prefix in main.ts`

--- server-side/src/main.ts
+++ server-side/src/main.ts
@@ -5,11 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist:true,
-    forbidNonWhitelisted:true,
-    transform:true
-  }))
+  // app.useGlobalPipes(new ValidationPipe({
+  //   whitelist:true,
+  //   forbidNonWhitelisted:true,
+  //   transform:true
+  // }))
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
